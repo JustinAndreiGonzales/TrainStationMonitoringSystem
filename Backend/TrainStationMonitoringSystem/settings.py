27770f3330d38e56b6f31225245af3499e74ef83
JustinAndreiGonzales <jcgonzales12@up.edu.ph>
2025-02-08 18:27:57 +0800

    added vercel deets

--- Backend/TrainStationMonitoringSystem/settings.py
+++ Backend/TrainStationMonitoringSystem/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-# import os
-# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +25,7 @@ SECRET_KEY = 'django-insecure-m519j$twvp!vv%^8$#ms4nk-3-s!-se8x3+ai1zqbpgt2g6g@w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
