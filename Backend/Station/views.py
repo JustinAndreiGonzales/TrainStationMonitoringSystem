c8718594fd197c7282fcf7629efe784504d30180
JustinAndreiGonzales <jcgonzales12@up.edu.ph>
2025-02-09 11:37:08 +0800

    edited error checking

--- Backend/Station/views.py
+++ Backend/Station/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from django.db import DatabaseError, OperationalError
+from django.db import OperationalError, connection
 
 from .models import Station
 from .serializers import StationSerializer
@@ -10,27 +10,35 @@ from .serializers import StationSerializer
 # Create your views here.
 @api_view(['GET'])
 def get_all_station(request):
-    try:
+    if check_database_status():
         stations = Station.objects.values('id', 'stationName', 'trainLine', 'isOperating')
         return Response(stations)
-    except OperationalError:
+    else:
         return Response(
             {"error": "Database is currently unavailable. Please try again later."},
             status=status.HTTP_503_SERVICE_UNAVAILABLE
         )
-    except DatabaseError:
-        return Response(
-            {"error": "Database error occured while fetching stations."},
-            status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
-    except Exception as e:
-        return Response(
-            {"error": f"An unexpected error occured: {str(e)}"},
-            status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
+    # except DatabaseError:
+    #     return Response(
+    #         {"error": "Database error occured while fetching stations."},
+    #         status=status.HTTP_500_INTERNAL_SERVER_ERROR
+    #     )
+    # except Exception as e:
+    #     return Response(
+    #         {"error": f"An unexpected error occured: {str(e)}"},
+    #         status=status.HTTP_500_INTERNAL_SERVER_ERROR
+    #     )
 
 @api_view(['GET'])
 def get_station(request, id):
     station = get_object_or_404(Station, id = id)
     serializer = StationSerializer(station)
     return Response(serializer.data)
+
+
+def check_database_status():
+    try:
+        connection.cursor()
+        return True
+    except OperationalError:
+        return False
