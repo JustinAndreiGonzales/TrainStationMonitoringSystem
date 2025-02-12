a8bd3f3a2fa3dbae076c7fc58faf8785667c16d1
eliiiphant <125492812+eliiiphant@users.noreply.github.com>
2025-02-12 20:34:49 +0800

    update: select station test cases

--- Playwright/tests/select_station.spec.ts
+++ Playwright/tests/select_station.spec.ts
@@ -1,6 +1,28 @@
 import { test, expect } from '@playwright/test';
 
-test('working station', async ({ page }) => {
+test('working station - lrt1', async ({ page }) => {
+  await page.goto('https://trenph.vercel.app/stations');
+
+  await page.locator('#train-lines').selectOption({label: 'LRT1'});
+  await page.locator('#stations').selectOption({label: 'United Nations'});
+  await page.getByRole('button').click();
+
+  await expect(page).toHaveURL("https://trenph.vercel.app/stations?id=13");
+});
+
+
+test('working station - lrt2', async ({ page }) => {
+  await page.goto('https://trenph.vercel.app/stations');
+
+  await page.locator('#train-lines').selectOption({label: 'LRT2'});
+  await page.locator('#stations').selectOption({label: 'Katipunan'});
+  await page.getByRole('button').click();
+
+  await expect(page).toHaveURL("https://trenph.vercel.app/stations?id=30");
+});
+
+
+test('working station - mrt3', async ({ page }) => {
   await page.goto('https://trenph.vercel.app/stations');
 
   await page.locator('#train-lines').selectOption({label: 'MRT3'});
