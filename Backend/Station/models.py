9a91afa3ef3cbdc907f39bf38416f431cbdaa43b
DelgadoDoan <125469582+DelgadoDoan@users.noreply.github.com>
2025-02-15 22:30:36 +0800

    Update models.py for Station

--- Backend/Station/models.py
+++ Backend/Station/models.py
@@ -14,8 +14,3 @@ class Station(models.Model):
     cctv = models.CharField(max_length=255)
     isOperating = models.BooleanField(default=True)
     stationIMG = models.CharField(max_length=255) # img url here; to change later (models.ImageField(upload_to=''))
-
-class Train(models.Model):
-    trainLine = models.CharField(max_length=255)
-    ocation = models.CharField(max_length=255)
-    currentStatus = models.CharField(max_length=255)
