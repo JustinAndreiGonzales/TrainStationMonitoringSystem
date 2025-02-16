49a4c40ffc48715c202a9e950b3676323475ee00
DelgadoDoan <125469582+DelgadoDoan@users.noreply.github.com>
2025-02-16 16:57:47 +0800

    Update admin.py

--- Backend/Users/admin.py
+++ Backend/Users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Admin
 
 # Register your models here.
+class AdminAdmin(UserAdmin):
+    model = Admin
+
+admin.site.register(Admin, AdminAdmin)
