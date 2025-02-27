7905248ec7dc09c056f7eed3689bb88a94f5d35a
eliiiphant <125492812+eliiiphant@users.noreply.github.com>
2025-02-27 11:51:51 +0800

    add: playwright tests

---
+++ Playwright/tests/2_announce_user.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+const url = 'https://trenph.vercel.app/';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(url);
+
+  await page.getByText('Announcements').click();
+  await page.waitForNavigation();
+
+  await page.getByText('Load more').click();
+  await page.getByText('Load more').click();
+});
+
+test('lrt 1 announcements', async ({ page }) => {
+  await page.isVisible("text='LRT-1'")
+});
+
+test('lrt 2 announcements', async ({ page }) => {
+  await page.isVisible("text='LRT-2'")
+});
+
+test('mrt 3 announcements', async ({ page }) => {
+  await page.isVisible("text='MRT-3'")
+});
