21d9137d3e1a11d91d41b150fc011d11ba9c8f41
DelgadoDoan <125469582+DelgadoDoan@users.noreply.github.com>
2025-03-03 11:16:00 +0800

    Update models.py
    Split 'cctv' field to left and right for tracking of each platform side.

--- Backend/Station/models.py
+++ Backend/Station/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 # Create your models here.
-
 class Station(models.Model):
     stationName = models.CharField(max_length=255)
     trainLine = models.CharField(max_length=255)
-    leftETA = models.IntegerField()
-    rightETA = models.IntegerField()
-    leftCurrentDensity = models.CharField(max_length=255)
-    rightCurrentDensity = models.CharField(max_length=255)
-    leftHistory = models.CharField(max_length=255) # to be changed
-    rightHistory = models.CharField(max_length=255) # to be changed
-    cctv = models.CharField(max_length=255)
+    leftETA = models.IntegerField(null=True)
+    rightETA = models.IntegerField(null=True)
+    leftCurrentDensity = models.CharField(max_length=255, null=True)
+    rightCurrentDensity = models.CharField(max_length=255, null=True)
+    leftHistory = models.CharField(max_length=255, null=True) # to be changed
+    rightHistory = models.CharField(max_length=255, null=True) # to be changed
+    leftCCTV = models.CharField(max_length=255, null=True)
+    rightCCTV = models.CharField(max_length=255, null=True)
     isOperating = models.BooleanField(default=True)
     stationIMG = models.CharField(max_length=255) # img url here; to change later (models.ImageField(upload_to=''))
