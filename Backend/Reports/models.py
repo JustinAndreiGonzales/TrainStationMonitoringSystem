8b9d58795e92369758cb1f01b94fe17bba7b3316
DelgadoDoan <125469582+DelgadoDoan@users.noreply.github.com>
2025-03-09 13:36:45 +0800

    Update models.py
    Updated datetime field for TDD

--- Backend/Reports/models.py
+++ Backend/Reports/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Reports(models.Model):
     subject = models.CharField(max_length=255)
     station = models.CharField(max_length=50)
     body = models.TextField()
-    datetimeReported = models.DateTimeField(auto_now_add=True)
+    datetimeReported = models.DateTimeField(default=timezone.now, editable=False)
