a3c8dcd3dbb74301779d67cf6e9017b204638b91
eliiiphant <125492812+eliiiphant@users.noreply.github.com>
2025-03-12 16:11:20 +0800

    add: updated stations page

--- frontend/src/lib/RadioButton.svelte
+++ frontend/src/lib/RadioButton.svelte
@@ -4,10 +4,10 @@
   export let selected;
 </script>
 
-<div class="inline-flex border-2 border-blue-600 rounded-lg overflow-hidden">
+<div class="inline-flex border-2 border-blue-600 rounded-lg overflow-hidden w-full">
   {#each values as val, i}
     <label
-      class="px-4 py-2 cursor-pointer transition-all duration-200 text-blue-600 border-blue-600"
+      class="flex-1 text-center px-4 py-2 cursor-pointer transition-all duration-200 text-blue-600 border-blue-600"
       class:bg-blue-600={selected === val}
       class:text-white={selected === val}
     >
