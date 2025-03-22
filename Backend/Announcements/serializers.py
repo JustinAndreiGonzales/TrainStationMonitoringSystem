c49af49bb3d1af89b43ae43ae80afd4efabdc999
JustinAndreiGonzales <jcgonzales12@up.edu.ph>
2025-03-22 23:55:45 +0800

    fixed announcements?

--- Backend/Announcements/serializers.py
+++ Backend/Announcements/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Announcements
 
 class AnnouncementSerializer(serializers.ModelSerializer):
+    tags =serializers.JSONField()
     class Meta:
         model = Announcements
         fields = '__all__'
