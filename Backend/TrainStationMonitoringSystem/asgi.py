1f1c5f9c914508c7d4b0e68a954903ed9f3c33e8
JustinAndreiGonzales <jcgonzales12@up.edu.ph>
2025-03-24 17:01:00 +0800

    changed procfile4

--- Backend/TrainStationMonitoringSystem/asgi.py
+++ Backend/TrainStationMonitoringSystem/asgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
-import Station.routing  # Replace "your_app" with your actual app name
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TrainStationMonitoringSystem.settings")
 
@@ -22,7 +22,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TrainStationMonitoringSystem.se
 #         "websocket": URLRouter(Station.routing.websocket_urlpatterns),  # Handles WebSockets
 #     }
 # )
+django.setup()
 
+import Station.routing  # Replace "your_app" with your actual app name
 application = ProtocolTypeRouter({
     "https": get_asgi_application(),
     "websocket": AllowedHostsOriginValidator(
