cd24eb2aeb5e935c6a802f170f488beba70b0fae
JustinAndreiGonzales <jcgonzales12@up.edu.ph>
2025-04-09 22:27:28 +0800

    fixed tests

--- Backend/Station/models.py
+++ Backend/Station/models.py
@@ -13,7 +13,6 @@ class Station(models.Model):
     leftCCTV = models.CharField(max_length=255, null=True)
     rightCCTV = models.CharField(max_length=255, null=True)
     isOperating = models.BooleanField(default=True)
-    stationIMG = models.CharField(max_length=255) # img url here; to change later (models.ImageField(upload_to=''))
     stationImage = models.TextField(max_length=255)
 
 
